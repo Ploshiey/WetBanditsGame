786e96d2c6d09f787d5d74242cdd79248650aa0b
Ploshiey <maxkelly04@gmail.com>
2024-09-02 11:28:01 +1000

    More tiles and fixes
    Added 3 more tiles and added borders and side trees to tiles. Also fixed issues with teleporting between tiles and camera

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -11,6 +11,9 @@ public class CameraScript : MonoBehaviour
     [SerializeField] Vector3 area0;
     [SerializeField] Vector3 area1;
     [SerializeField] Vector3 area2;
+    [SerializeField] Vector3 area3;
+    [SerializeField] Vector3 area4;
+    [SerializeField] Vector3 area5;
 
 
     void Start()
@@ -32,14 +35,29 @@ public class CameraScript : MonoBehaviour
                 camPos = area0;
                 this.transform.position = camPos;
                 break;
-
+                
             case 1:
                 camPos = area1;
                 this.transform.position = camPos;
                 break;
-
+                
             case 2:
                 camPos = area2;
+                this.transform.position = camPos;
+                break;
+                
+            case 3:
+                camPos = area3;
+                this.transform.position = camPos;
+                break;
+                
+            case 4:
+                camPos = area4;
+                this.transform.position = camPos;
+                break;
+                
+            case 5:
+                camPos = area5;
                 this.transform.position = camPos;
                 break;
 
