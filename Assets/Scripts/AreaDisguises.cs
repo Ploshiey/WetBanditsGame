75bbf701317f116e234da51ffb611a80edad569c
Ploshiey <maxkelly04@gmail.com>
2024-09-24 16:44:36 +1000

    Goober v10.0
    Goober is goated with the sauce now. Man will wait for u to get near then pounce, once discovered, it will hunt the player down, following them as long as they are in the same tile or an adjacent tile. Once the player loses the goober, it will go back to hiding in the tile it it currently in... waiting for its next victim

--- Assets/Scripts/AreaDisguises.cs
+++ Assets/Scripts/AreaDisguises.cs
@@ -6,7 +6,7 @@ using UnityEngine.ProBuilder.Shapes;
 
 public class AreaDisguises : MonoBehaviour
 {
-    public Goober goob;
+    [SerializeField] Goober goob;
     [SerializeField] GameObject goober;
     [SerializeField] GameObject AllGoober;
     [SerializeField] GameObject phoney;
@@ -15,12 +15,44 @@ public class AreaDisguises : MonoBehaviour
     private int DisObj;
     private float x;
     private float z;
+    public int temp;
+
+    private void Start()
+    {
+        temp = Random.Range(1, 5);
+        areaHidables(temp);
+        goobystarter(temp);
+    }
+
+    public void goobystarter(int value)
+    {
+        goob.gooberPozUpdater(value);
+    }
     public void areaHidables(int area)
     {
         phoney.SetActive(true);
         switch (area)
         {
             case 0:
+                 
+                break;
+            case 1:
+                DisObj = Random.Range(0, 3);
+                curMat = mats[DisObj];
+                phoney.GetComponent<MeshRenderer>().material = curMat;
+                x = Random.Range(-2.5f, 15.5f);
+                z = Random.Range(0.5f, 6f);
+                AllGoober.transform.position = new Vector3(x, 1, z);
+                break;
+            case 2:
+                DisObj = Random.Range(0, 3);
+                curMat = mats[DisObj];
+                phoney.GetComponent<MeshRenderer>().material = curMat;
+                x = Random.Range(27.5f, 45.5f);
+                z = Random.Range(0.5f, 6f);
+                AllGoober.transform.position = new Vector3(x, 1, z);
+                break;
+            case 3:
                 DisObj = Random.Range(0, 3);
                 curMat = mats[DisObj];
                 phoney.GetComponent<MeshRenderer>().material = curMat;
@@ -28,7 +60,24 @@ public class AreaDisguises : MonoBehaviour
                 z = Random.Range(31f, 37.5f);
                 AllGoober.transform.position = new Vector3(x, 1, z);
                 break;
-             
+            case 4:
+                DisObj = Random.Range(0, 3);
+                curMat = mats[DisObj];
+                phoney.GetComponent<MeshRenderer>().material = curMat;
+                x = Random.Range(-2.5f, 15.5f);
+                z = Random.Range(31f, 37.5f);
+                AllGoober.transform.position = new Vector3(x, 1, z);
+                break;
+            case 5:
+                DisObj = Random.Range(0, 3);
+                curMat = mats[DisObj];
+                phoney.GetComponent<MeshRenderer>().material = curMat;
+                x = Random.Range(27.5f, 45.5f);
+                z = Random.Range(31f, 37.5f);
+                AllGoober.transform.position = new Vector3(x, 1, z);
+                break;
+
+
             default:
                  
                 break;
--- Assets/Scripts/Goober.cs
+++ Assets/Scripts/Goober.cs
@@ -2,37 +2,99 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
 using UnityEngine.SceneManagement;
+
+/*
+    [p12, p13, p14, p15, p8, p9, p10, p11, p4, p5, p6, p7, p0, p1, p2, p3]
+ 
+ 
+ 
+*/
+
+
 
 public class Goober : MonoBehaviour
 {
     public AreaDisguises areaDisguises;
     public CameraScript camscript;
+    public Borders border;
     private int gooberPos;
     [SerializeField] GameObject phoney;
     public bool discovered;
     [SerializeField] GameObject player;
     [SerializeField] GameObject goober;
     [SerializeField] private int speed = 3;
+    [SerializeField] private Vector3[] goToPoz;
     [SerializeField] private AudioSource Audable;
+    private int GoobLocat;
+    private int locat;
     // Start is called before the first frame update
     void Start()
     {
-        areaDisguises.areaHidables(0);
         goober.GetComponent<SpriteRenderer>().enabled = false;
+        
     }
 
-    // Update is called once per frame
+    public void playerPozUpdater(int value)
+    {
+        locat = value;
+    }
+    public void gooberPozUpdater(int value)
+    {
+        GoobLocat = value;
+    }
+
+
     void Update()
     {
-        if (discovered == true)
+        
+        if (discovered == true && locat == GoobLocat)
         {
             Vector3 localPosition = player.transform.position - transform.position;
             localPosition = localPosition.normalized;
             transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
-            
-            
         }
+        else if(discovered == true && GoobLocat != locat )
+        {
+            if (locat - GoobLocat == 1 && GoobLocat != 2)
+            {
+                Vector3 localPosition = goToPoz[GoobLocat + 1] - transform.position;
+                localPosition = localPosition.normalized;
+                transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
+            }
+            else if (GoobLocat - locat - 3 == 0 && GoobLocat != 4)
+            {
+                Vector3 localPosition = goToPoz[GoobLocat - 3] - transform.position;
+                localPosition = localPosition.normalized;
+                transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
+            }
+            else if (GoobLocat - locat == 1 && GoobLocat != 3)
+            {
+                Vector3 localPosition = goToPoz[GoobLocat - 1] - transform.position;
+                localPosition = localPosition.normalized;
+                transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
+            }
+                
+            else if(locat - GoobLocat - 3 == 0 && GoobLocat != 1)
+            {
+                Vector3 localPosition = goToPoz[GoobLocat + 3] - transform.position;
+                localPosition = localPosition.normalized;
+                transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
+            }
+            
+            
+            
+            
+            else
+            {
+                discovered = false;
+                Audable.enabled = false;
+                goober.GetComponent<SpriteRenderer>().enabled = false;
+                areaDisguises.areaHidables(GoobLocat);
+            }
+        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -42,6 +104,7 @@ public class Goober : MonoBehaviour
             phoney.SetActive(false);
             goober.GetComponent<SpriteRenderer>().enabled = true;
             discovered = true;
+            Audable.enabled = true;
             Audable.Play();
         }
     }
