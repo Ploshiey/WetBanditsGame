d3dc644ad63fa0b4b7777cf2526c3aa5d9a119c9
Ploshiey <maxkelly04@gmail.com>
2024-10-24 16:57:49 +1100

    Update goober code

--- Assets/Goober/Goober.cs
+++ Assets/Goober/Goober.cs
@@ -20,13 +20,13 @@ public class Goober : MonoBehaviour
     private int GoobLocat;
     private int GoobWanderLocat;
     private int locat;
-    private bool isWandering;
+    private bool goobOnTheMove;
     private int randMove;
     // Start is called before the first frame update
     void Start()
     {
         goober.GetComponent<SpriteRenderer>().enabled = false;
-        isWandering = false;
+        goobOnTheMove = false;
 
     }
 
@@ -42,14 +42,16 @@ public class Goober : MonoBehaviour
 
     void Update()
     {
-
-        if (discovered == true && locat == GoobLocat && isWandering == false)
+        #region goober chasing in same tile
+        if (discovered == true && locat == GoobLocat && goobOnTheMove == false)
         {
             Vector3 localPosition = player.transform.position - transform.position;
             localPosition = localPosition.normalized;
             transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
         }
-        else if (discovered == true && GoobLocat != locat && isWandering == false)
+        #endregion
+        #region goober follow to next tile
+        else if (discovered == true && GoobLocat != locat && goobOnTheMove == false)
         {
             //if player is in the tile to the right of the Goober
             if (locat - GoobLocat == 1 && (GoobLocat != 3 || GoobLocat != 7 || GoobLocat != 11 || GoobLocat != 15))
@@ -79,64 +81,80 @@ public class Goober : MonoBehaviour
                 localPosition = localPosition.normalized;
                 transform.Translate(localPosition.x * Time.deltaTime * speed, 0f * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
             }
-
-            else
+            #endregion
+        }
+        #region goober wandering around
+        #region goober selecting new wander target
+        else if (!(locat - GoobLocat == 1 && (GoobLocat != 3 || GoobLocat != 7 || GoobLocat != 11 || GoobLocat != 15)) && !(GoobLocat - locat - 4 == 0 && (GoobLocat != 0)) &&
+                !(GoobLocat - locat == 1 && (GoobLocat != 4 || GoobLocat != 8 || GoobLocat != 12)) && !(locat - GoobLocat - 4 == 0 && GoobLocat != 1) && !discovered && goobOnTheMove)
+        {
+            StartCoroutine(waitForGoobMove());
+            if (GoobLocat == GoobWanderLocat && goobOnTheMove == true)
             {
-                isWandering = true;
-                if (GoobLocat == GoobWanderLocat && isWandering == true)
+                randMove = Random.Range(0, 4);
+                switch (randMove)
                 {
-                    randMove = Random.Range(0, 4);
-                    switch (randMove)
-                    {
-                        case 0:
-                            if (GoobLocat != 4 || GoobLocat != 8 || GoobLocat != 12 || GoobLocat != 0)
-                            {
-                                GoobWanderLocat = GoobLocat - 1;
-                            }
-                            break;
-                        case 1:
-                            if (GoobLocat != 12 || GoobLocat != 13 || GoobLocat != 14 || GoobLocat != 15)
-                            {
-                                GoobWanderLocat = GoobLocat + 4;
-                            }
-                            break;
-                        case 2:
-                            if (GoobLocat != 3 || GoobLocat != 7 || GoobLocat != 11 || GoobLocat != 15)
-                            {
-                                GoobWanderLocat = GoobLocat + 1;
-                            }
-                            break;
-                        case 3:
-                            if (GoobLocat != 0 || GoobLocat != 1 || GoobLocat != 2 || GoobLocat != 3)
-                            {
-                                GoobWanderLocat = GoobLocat - 4;
-                            }
-                            break;
-                        default:
-                            break;
-                    }
+                    case 0:
+                        if (GoobLocat != 4 || GoobLocat != 8 || GoobLocat != 12 || GoobLocat != 0)
+                        {
+                            GoobWanderLocat = GoobLocat - 1;
+                        }
+                        break;
+                    case 1:
+                        if (GoobLocat != 12 || GoobLocat != 13 || GoobLocat != 14 || GoobLocat != 15)
+                        {
+                            GoobWanderLocat = GoobLocat + 4;
+                        }
+                        break;
+                    case 2:
+                        if (GoobLocat != 3 || GoobLocat != 7 || GoobLocat != 11 || GoobLocat != 15)
+                        {
+                            GoobWanderLocat = GoobLocat + 1;
+                        }
+                        break;
+                    case 3:
+                        if (GoobLocat != 0 || GoobLocat != 1 || GoobLocat != 2 || GoobLocat != 3)
+                        {
+                            GoobWanderLocat = GoobLocat - 4;
+                        }
+                        break;
+                    default:
+                        break;
                 }
-                else if (GoobLocat != GoobWanderLocat && isWandering == true)
-                {
-                    Vector3 localPosition = goToPoz[GoobWanderLocat] - transform.position;
-                    localPosition = localPosition.normalized;
-                    transform.Translate(localPosition.x * Time.deltaTime * speed, 0f * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
-                }
+            }
+            #endregion
+            #region goober wandering towards rendom target tile
+            else if (GoobLocat != GoobWanderLocat && goobOnTheMove == true)
+            {
+                Vector3 localPosition = goToPoz[GoobWanderLocat] - transform.position;
+                localPosition = localPosition.normalized;
+                transform.Translate(localPosition.x * Time.deltaTime * speed, 0f * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
+            }
+            #endregion
+            #endregion
+        else if (!(locat - GoobLocat == 1 && (GoobLocat != 3 || GoobLocat != 7 || GoobLocat != 11 || GoobLocat != 15)) && !(GoobLocat - locat - 4 == 0 && (GoobLocat != 0)) &&
+                !(GoobLocat - locat == 1 && (GoobLocat != 4 || GoobLocat != 8 || GoobLocat != 12)) && !(locat - GoobLocat - 4 == 0 && GoobLocat != 1) && !discovered)
+            {
+                discovered = false;
+                Audable.enabled = false;
+                goober.GetComponent<SpriteRenderer>().enabled = false;
+                areaDisguises.areaHidables(GoobLocat);
+
             }
 
 
 
 
-
-            //discovered = false;
-            //Audable.enabled = false;
-            //goober.GetComponent<SpriteRenderer>().enabled = false;
-            //areaDisguises.areaHidables(GoobLocat);
         }
 
     }
 
-
+    private IEnumerator waitForGoobMove()
+    {
+        yield return new WaitForSeconds(20);
+        goobOnTheMove = true;
+        discovered = false;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player" && discovered == false)
