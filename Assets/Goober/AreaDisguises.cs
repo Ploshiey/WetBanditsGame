c0537bea6d55d2afc938b040c246d49a1c626f10
Ploshiey <maxkelly04@gmail.com>
2024-10-29 10:09:07 +1100

    Fixed phoney goober
    Goober can hide as multiple different things now

--- Assets/Goober/AreaDisguises.cs
+++ Assets/Goober/AreaDisguises.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
-using UnityEngine.ProBuilder.Shapes;
 
 public class AreaDisguises : MonoBehaviour
 {
@@ -10,8 +9,8 @@ public class AreaDisguises : MonoBehaviour
     [SerializeField] GameObject goober;
     [SerializeField] GameObject AllGoober;
     [SerializeField] GameObject phoney;
-    [SerializeField] Material[] mats;
-    private Material curMat;
+    [SerializeField] Sprite[] mats;
+    private Sprite curMat;
     private int DisObj;
     private float x;
     private float z;
@@ -37,121 +36,121 @@ public class AreaDisguises : MonoBehaviour
                  
                 break;
             case 1:
-                DisObj = Random.Range(0, 4);
+                DisObj = Random.Range(0, 8);
                 curMat = mats[DisObj];
-                phoney.GetComponent<MeshRenderer>().material = curMat;
+                phoney.GetComponent<SpriteRenderer>().sprite = curMat;
                 x = Random.Range(-101f, -79f);
                 z = Random.Range(-1f, 11f);
                 AllGoober.transform.position = new Vector3(x, -48, z);
                 break;
             case 2:
-                DisObj = Random.Range(0, 4);
+                DisObj = Random.Range(0, 8);
                 curMat = mats[DisObj];
-                phoney.GetComponent<MeshRenderer>().material = curMat;
+                phoney.GetComponent<SpriteRenderer>().sprite = curMat;
                 x = Random.Range(-1f, 21.5f);
                 z = Random.Range(-1f, 11f);
                 AllGoober.transform.position = new Vector3(x, -48, z);
                 break;
             case 3:
-                DisObj = Random.Range(0, 4);
+                DisObj = Random.Range(0, 8);
                 curMat = mats[DisObj];
-                phoney.GetComponent<MeshRenderer>().material = curMat;
+                phoney.GetComponent<SpriteRenderer>().sprite = curMat;
                 x = Random.Range(99f, 116.5f);
                 z = Random.Range(-1f, 11f);
                 AllGoober.transform.position = new Vector3(x, -48, z);
                 break;
             case 4:
-                DisObj = Random.Range(0, 4);
+                DisObj = Random.Range(0, 8);
                 curMat = mats[DisObj];
-                phoney.GetComponent<MeshRenderer>().material = curMat;
+                phoney.GetComponent<SpriteRenderer>().sprite = curMat;
                 x = Random.Range(-198f, -178f);
                 z = Random.Range(99f, 111f);
                 AllGoober.transform.position = new Vector3(x, 2, z);
                 break;
             case 5:
-                DisObj = Random.Range(0, 4);
+                DisObj = Random.Range(0, 8);
                 curMat = mats[DisObj];
-                phoney.GetComponent<MeshRenderer>().material = curMat;
+                phoney.GetComponent<SpriteRenderer>().sprite = curMat;
                 x = Random.Range(-101f, -78f);
                 z = Random.Range(99f, 111f);
                 AllGoober.transform.position = new Vector3(x, 2, z);
                 break;
             case 6:
-                DisObj = Random.Range(0, 4);
+                DisObj = Random.Range(0, 8);
                 curMat = mats[DisObj];
-                phoney.GetComponent<MeshRenderer>().material = curMat;
+                phoney.GetComponent<SpriteRenderer>().sprite = curMat;
                 x = Random.Range(-1f, 21.5f);
                 z = Random.Range(99f, 111f);
                 AllGoober.transform.position = new Vector3(x, 2, z);
                 break;
             case 7:
-                DisObj = Random.Range(0, 4);
+                DisObj = Random.Range(0, 8);
                 curMat = mats[DisObj];
-                phoney.GetComponent<MeshRenderer>().material = curMat;
+                phoney.GetComponent<SpriteRenderer>().sprite = curMat;
                 x = Random.Range(99f, 116.5f);
                 z = Random.Range(99f, 111f);
                 AllGoober.transform.position = new Vector3(x, 2, z);
                 break;
             case 8:
-                DisObj = Random.Range(0, 4);
+                DisObj = Random.Range(0, 8);
                 curMat = mats[DisObj];
-                phoney.GetComponent<MeshRenderer>().material = curMat;
+                phoney.GetComponent<SpriteRenderer>().sprite = curMat;
                 x = Random.Range(-198f, -178f);
                 z = Random.Range(198f, 211f);
                 AllGoober.transform.position = new Vector3(x, 52, z);
                 break;
             case 9:
-                DisObj = Random.Range(0, 4);
+                DisObj = Random.Range(0, 8);
                 curMat = mats[DisObj];
-                phoney.GetComponent<MeshRenderer>().material = curMat;
+                phoney.GetComponent<SpriteRenderer>().sprite = curMat;
                 x = Random.Range(-101f, -78f);
                 z = Random.Range(198f, 211f);
                 AllGoober.transform.position = new Vector3(x, 52, z);
                 break;
             case 10:
-                DisObj = Random.Range(0, 4);
+                DisObj = Random.Range(0, 8);
                 curMat = mats[DisObj];
-                phoney.GetComponent<MeshRenderer>().material = curMat;
+                phoney.GetComponent<SpriteRenderer>().sprite = curMat;
                 x = Random.Range(-1f, 21.5f);
                 z = Random.Range(198f, 211f);
                 AllGoober.transform.position = new Vector3(x, 52, z);
                 break;
             case 11:
-                DisObj = Random.Range(0, 4);
+                DisObj = Random.Range(0, 8);
                 curMat = mats[DisObj];
-                phoney.GetComponent<MeshRenderer>().material = curMat;
+                phoney.GetComponent<SpriteRenderer>().sprite = curMat;
                 x = Random.Range(99f, 116.5f);
                 z = Random.Range(198f, 211f);
                 AllGoober.transform.position = new Vector3(x, 52, z);
                 break;
             case 12:
-                DisObj = Random.Range(0, 4);
+                DisObj = Random.Range(0, 8);
                 curMat = mats[DisObj];
-                phoney.GetComponent<MeshRenderer>().material = curMat;
+                phoney.GetComponent<SpriteRenderer>().sprite = curMat;
                 x = Random.Range(-198f, -178f);
                 z = Random.Range(298f, 311f);
                 AllGoober.transform.position = new Vector3(x, 102, z);
                 break;
             case 13:
-                DisObj = Random.Range(0, 4);
+                DisObj = Random.Range(0, 8);
                 curMat = mats[DisObj];
-                phoney.GetComponent<MeshRenderer>().material = curMat;
+                phoney.GetComponent<SpriteRenderer>().sprite = curMat;
                 x = Random.Range(-101f, -78f);
                 z = Random.Range(298f, 311f);
                 AllGoober.transform.position = new Vector3(x, 102, z);
                 break;
             case 14:
-                DisObj = Random.Range(0, 4);
+                DisObj = Random.Range(0, 8);
                 curMat = mats[DisObj];
-                phoney.GetComponent<MeshRenderer>().material = curMat;
+                phoney.GetComponent<SpriteRenderer>().sprite = curMat;
                 x = Random.Range(-1f, 21.5f);
                 z = Random.Range(298f, 311f);
                 AllGoober.transform.position = new Vector3(x, 102, z);
                 break;
             case 15:
-                DisObj = Random.Range(0, 4);
+                DisObj = Random.Range(0, 8);
                 curMat = mats[DisObj];
-                phoney.GetComponent<MeshRenderer>().material = curMat;
+                phoney.GetComponent<SpriteRenderer>().sprite = curMat;
                 x = Random.Range(99f, 116.5f);
                 z = Random.Range(298f, 311f);
                 AllGoober.transform.position = new Vector3(x, 102, z);
